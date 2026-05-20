cf6f6d9b9fb8d3a7c817f2028204ddfa28d0e115
Nicfem <sosiska2004dota2@gmail.com>
2026-05-20 15:31:23 +0300

    fix: disable hi-res scroll events for X68HE mouse.

--- nix-os/modules/drivers.nix
+++ nix-os/modules/drivers.nix
@@ -1,4 +1,12 @@
 {
+  # Fix scroll/motion event swap bug for X68HE Mouse (usb:3151:502d)
+  environment.etc."libinput/local-overrides.quirks".text = ''
+    [X68HE Mouse scroll fix]
+    MatchUdevType=mouse
+    MatchName=*X68HE Mouse
+    AttrEventCode=-EV_REL:0x0b;-EV_REL:0x0c
+  '';
+
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware.graphics = {
