946403f5c9ba9d549ca07e79c4e0b1f716ad68a8
Nicfem <sosiska2004dota2@gmail.com>
2026-05-20 09:27:04 +0300

    feat: set nvim as defaut editor.

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -9,6 +9,8 @@
   home.username = user;
   home.homeDirectory = "/home/${user}";
 
+  home.sessionVariables.EDITOR = "nvim";
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
--- home-manager/modules/programs/git.nix
+++ home-manager/modules/programs/git.nix
@@ -3,5 +3,6 @@
     enable = true;
     settings.user.name = "Nicfem";
     settings.user.email = "sosiska2004dota2@gmail.com";
+    settings.core.editor = "nvim";
   };
 }
