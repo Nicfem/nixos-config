0453fd794dfec6e202dce7b46534c4865500735e
Nicfem <sosiska2004dota2@gmail.com>
2026-05-29 08:37:45 +0300

    feat: add config file for tweakcc patch.

--- nix-os/modules/claude-code-patch.nix
+++ nix-os/modules/claude-code-patch.nix
@@ -1,18 +1,19 @@
 { pkgs, inputs, ... }:
 
 let
-  tweakcc = inputs.tweakcc.packages.${pkgs.system}.tweakcc;
-
+  tweakcc = inputs.tweakcc.packages.${pkgs.system}.default;
 in
 {
   nixpkgs.overlays = [
     (_final: prev: {
-      tweakcc = tweakcc;
-      
       claude-code = prev.claude-code.overrideAttrs (old: {
         nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ tweakcc ];
 
         postInstall = (old.postInstall or "") + ''
+          export HOME=$(mktemp -d)
+          export TWEAKCC_CONFIG_DIR=$(mktemp -d)
+          cp --no-preserve=mode ${inputs.tweakcc}/config.json "$TWEAKCC_CONFIG_DIR/config.json"
+
           TWEAKCC_CC_INSTALLATION_PATH="$out/bin/claude" tweakcc --apply
         '';
       });
